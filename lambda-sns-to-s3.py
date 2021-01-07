65002c31ae17177cdabb6bfd3706c16c40ba6f73
chkp-rolandop <71713130+chkp-rolandop@users.noreply.github.com>
2021-01-07 12:35:48 -0500

    Update lambda-sns-to-s3.py

--- lambda-sns-to-s3.py
+++ lambda-sns-to-s3.py
@@ -3,15 +3,23 @@ import boto3
 print('Loading function')
 
 def lambda_handler(event, context):
+    #s3prefix is the prefix will be used in the filename
     s3prefix = 'CloudGuardAlerts-'
+    
+    #ext is the extension for the filename
     ext = '.json'
+    
+    #s3_bucket is the name of the s3 bucket to write the files to THIS VALUE MUST BE CHANGED
     s3_bucket = 'your-s3-bucket'
     
+    #Pulling timestamp and message from SNS message
     timestamp = event['Records'][0]['Sns']['Timestamp']
     message = event['Records'][0]['Sns']['Message']
     
+    #Building Filename
     filename = s3prefix + timestamp + ext
 
+    #Writing message to s3 bucket using filename created
     try:
         s3 = boto3.client('s3')
         s3.put_object(Bucket=s3_bucket, Key=filename, Body=message)
